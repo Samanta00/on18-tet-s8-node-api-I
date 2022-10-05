0983f96e9b8f82fc9aba4b2ef5c76ed20712b9c4
Ellen Samanta <ellen.samanta@outlook.com>
2022-10-05 10:34:48 -0300

    ellen-samanta

---
+++ server.js
@@ -0,0 +1,43 @@
+const express=require('express')
+const app=express()
+const port=process.env.PORT || 8080
+
+
+
+function bancoDeDados() {
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            return resolve({
+                series: require("./data/series.json"),
+                filmes: require("./data/filmes.json")
+            })
+                
+        }, 1500);
+    })
+}
+  
+  
+app.get('/menu',(req,res)=>{
+    res.status(200).json({
+        mensage:'Bem vindo ao Canal de séries e filmes'})
+})
+
+app.get('/filmes', async(req,res)=>{
+    let bancoFilmes=await bancoDeDados()
+    res.status(200).send(bancoFilmes.filmes)
+  
+})
+app.get('/series', async(req,res)=>{
+    let bancoCategorias=await bancoDeDados()
+    res.status(200).send(bancoCategorias.series)
+})
+
+app.post('/filmes',(req,res)=>{
+    let bancoFilmes=await bancoDeDados()
+    bancoFilmes.filmes.push(req.body)
+    res.status(200).send('Filme foi cadastrado com sucesso')
+})
+
+app.listen(port,()=>{
+    console.log('servidor esta rodando')
+})
